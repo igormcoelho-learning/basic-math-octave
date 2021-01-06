be4fa0aef33612cc963c5bc72378d548a44e24a2
Igor Machado Coelho <igor.machado@gmail.com>
2021-01-06 12:08:05 -0300

    Update student.m

--- student.m
+++ student.m
@@ -9,6 +9,9 @@ function sol = exercise1()
    syms x;                       # declare symbolic variable x
    dh = 2*x+1;                   # solution to exercise one: h'(x) = 2x+1
    sol = function_handle (dh);   # converts symbolic into a real function
+   # other option, just use 'diff' function from octave
+   # h = x^2 + x
+   # sol = function_handle( diff(h) )
 endfunction
 
 # Exercise 2: write the correct answer on variable 'sol'

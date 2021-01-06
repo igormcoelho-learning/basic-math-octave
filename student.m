d0196fbc07135097145f71ae18b562ed83b00145
Igor Machado Coelho <igor.machado@gmail.com>
2021-01-06 01:26:51 -0300

    Create student.m

---
+++ student.m
@@ -0,0 +1,17 @@
+# This is a student file, feel free to put solutions on every exercise
+
+1;
+
+pkg load symbolic;
+
+# Exercise 1: write the correct answer on variable 'sol'
+function sol = exercise1()
+   syms x;                       # declare symbolic variable x
+   dh = 2*x+1;                   # solution to exercise one: h'(x) = 2x+1
+   sol = function_handle (dh);   # converts symbolic into a real function
+endfunction
+
+# Exercise 2: write the correct answer on variable 'sol'
+function sol = exercise2()
+   # no solution yet... you need to fix this!
+endfunction
